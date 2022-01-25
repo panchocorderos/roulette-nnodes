af0b53a77d0bd3619c99ac9b5c6b7501b55795d0
panchocorderos <fco.cordero98@gmail.com>
2022-01-25 14:50:45 -0300

    add: add lib path to the autoloader

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module RouletteNnodes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
+    config.load_defaults 7.0 
+    config.autoload_paths << "#{root}/lib/utilities"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
